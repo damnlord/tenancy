a76d24f4689b501e64a9d2b6aec3c6b694c1412c
damnlord <stanislav.sv.georgiev@gmail.com>
2024-04-15 06:27:23 +0000

    composer update

--- app/Jobs/CreateFrameworkDirectoriesForTenant.php
+++ app/Jobs/CreateFrameworkDirectoriesForTenant.php
@@ -18,7 +18,9 @@ class CreateFrameworkDirectoriesForTenant
         $this->tenant->run(function ($tenant) {
             $storage_path = storage_path();
 
-            mkdir("$storage_path/framework/cache", 0777, true);
+            if (!file_exists("$storage_path/framework/cache")) {
+                mkdir("$storage_path/framework/cache", 0777, true);
+            }
         });
     }
 }
